a15306753bb3ccf09cce1df8432730af76ac7d09
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 03:26:14 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,7 +22,7 @@ def third_challenge
       :heroine => {
        name: "Juliet", age: "15", status: "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [{name: "Benvolio", age: "17", attitude: "worried"}, ]
    }
   }
 
