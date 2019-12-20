455be69743942ed78d980cab6caa681ea9170d97
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-20 03:20:03 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,14 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       #your key/value pairs here!
       }, 
     :capulet => {
+      
       #your key/value pairs here!
       }}
 
